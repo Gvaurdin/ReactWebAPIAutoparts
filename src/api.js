b5ff201c74f569e689ffbbb138be1a82f561e898
Stanislav <gvaurdin777@yandex.ru>
2025-03-31 00:36:19 +0300

    Change baseURL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080'  // Используйте имя контейнера PHP API
+    baseURL: 'http://185.50.203.159'
 });
 
 export const fetchProducts = () => api.get('/products');
