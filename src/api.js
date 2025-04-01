84113b585f0b929741f65c1811ce15bffa1041eb
Stanislav <gvaurdin777@yandex.ru>
2025-04-01 13:20:56 +0300

    Change base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://185.50.203.159/api'
+    baseURL: 'https://gsa-top.ru/api'
 });
 
 export const fetchProducts = () => api.get('/products');
